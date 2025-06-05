1d9322c98db0c04d54f1ee7daf509b050073d4b8
Oyale <salamivictor15@gmail.com>
2025-06-05 15:06:42 +0100

    Adds scopable environments to the interpreter and support for statement blocks

--- Jlox/src/Environment.java
+++ Jlox/src/Environment.java
@@ -5,7 +5,18 @@ import java.util.Map;
 
 class Environment
 {
+	final Environment enclosing;
 	private final Map<String, Object> values = new HashMap<>();
+
+	Environment()
+	{
+		enclosing = null;
+	}
+
+	Environment(Environment enclosing)
+	{
+		this.enclosing = enclosing;
+	}
 
 	Object get(Token name)
 	{
@@ -13,6 +24,8 @@ class Environment
 		{
 			return values.get(name.lexeme);
 		}
+
+		if(enclosing != null) return enclosing.get(name);
 
 		throw new RuntimeError(name, "Undefined variable ' " + name.lexeme + " '.");
 	}
@@ -24,6 +37,13 @@ class Environment
 			values.put(name.lexeme, value);
 			return;
 		}
+
+		if (enclosing != null)
+		{
+			enclosing.assign(name, value);
+			return;
+		}
+
 
 		throw new RuntimeError(name, "Undefined variable ' " + name.lexeme + " '.");
 	}
